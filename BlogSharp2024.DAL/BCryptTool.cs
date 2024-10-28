a217afa06180c52f752022311c0052436e32432e
Jakob Farian Krarup <kr@rup.dk>
2024-10-28 12:33:31 +0100

    BCrypt tool added

---
+++ BlogSharp2024.DAL/BCryptTool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bcryptnet = BCrypt.Net;
+
+namespace BlogSharp2024.DAL
+{
+    public class BCryptTool
+    {
+        public static string HashPassword(string password) => bcryptnet.BCrypt.HashPassword(password, GetRandomSalt());
+        public static bool ValidatePassword(string password, string correctHash) => bcryptnet.BCrypt.Verify(password, correctHash);
+        private static string GetRandomSalt() => bcryptnet.BCrypt.GenerateSalt(12);
+    }
+
+}

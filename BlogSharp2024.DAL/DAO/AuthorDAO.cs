ad72ff3ecddcfc535b568320169ba41e1600fd43
Jakob Farian Krarup <kr@rup.dk>
2024-10-28 13:14:11 +0100

    Login implemented

--- BlogSharp2024.DAL/DAO/AuthorDAO.cs
+++ BlogSharp2024.DAL/DAO/AuthorDAO.cs
@@ -11,6 +11,7 @@ namespace BlogSharp2024.DAL.DAO
     public class AuthorDAO : BaseDAO, IAuthorDAO
     {
         private const string INSERT_SQL = "INSERT INTO Author (Email, BlogTitle, PasswordHash) OUTPUT INSERTED.Id  VALUES (@Email, @BlogTitle, @PasswordHash);";
+            private const string LOGIN_SQL = "SELECT Id, PasswordHash FROM Author WHERE Email=@Email";
         public AuthorDAO(string connectionString) : base(connectionString)
         {
         }
@@ -34,12 +35,20 @@ namespace BlogSharp2024.DAL.DAO
         {
             var passwordHash = BCryptTool.HashPassword(author.Password);
             using var connection = CreateConnection();
-            return connection.QuerySingle<int>(INSERT_SQL, new { Email = author.Email, BlogTitle = author.BlogTitle, PasswordHash = passwordHash }); //can be simplified
+            return connection.QuerySingle<int>(INSERT_SQL, new { author.Email, author.BlogTitle, PasswordHash = passwordHash }); //can be simplified
         }
 
         public int TryLogin(string email, string password)
         {
-            throw new NotImplementedException();
+            // public int Login (string email, string password) 
+            using var connection = CreateConnection();
+
+            // get id and password has
+            var authorTuple = connection.QueryFirstOrDefault<AuthorTuple>(LOGIN_SQL, 		new { Email = email});
+            // validate hash by salting, rehashing and comparing with stored version
+            if (authorTuple != null && BCryptTool.ValidatePassword(password, authorTuple.PasswordHash)){    return authorTuple.Id;}
+            return -1;
+
         }
 
         public bool Update(Author account)
---
+++ BlogSharp2024.DAL/DAO/AuthorTuple.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogSharp2024.DAL.DAO
+{
+    internal class AuthorTuple
+    {
+        public int Id { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
